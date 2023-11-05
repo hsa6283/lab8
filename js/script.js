5fc836a2509e9b862d6a608a9f333ec5c0dabc87
H <80310260+hsa6283@users.noreply.github.com>
2023-11-04 21:10:50 -0500

    fix error2

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 function init(){
     //add your javascrip between these two lines of code
     var button = document.getElementById('entrybutton');
-    function showMeText(){
-      var textbox = document.getElementById('entryinput');
+    function display(){
       document.getElementById('textoutput').innerHTML = textbox.value;
-      alert("Seung-A Ham: " + textbox.value);
     }
-    button.addEventListener('click', showMeText);
+    var textbox = document.getElementById('entryinput');
+    alert("Seung-A Ham: " + textbox.value);
+    button.addEventListener('click', display);
     }
 window.addEventListener('load', init);
