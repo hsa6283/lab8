8ea8c41c7c63570d9d24f82f51780f7591056a4a
H <80310260+hsa6283@users.noreply.github.com>
2023-11-04 21:26:09 -0500

    fix 3

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 function init(){
     //add your javascrip between these two lines of code
     function eventFunc(){
+      var name = 'Seung-A Ham: ';
+      var text = document.getElementById('entryinput');
+      var result = document.getElementById('textoutput');
       alert(name+text.value);
       result.innerHTML = text.value;
     }
-    var name = 'Seung-A Ham: ';
     var but = document.getElementById('entrybutton');
-    var text = document.getElementById('entryinput');
-    var result = document.getElementById('textoutput');
     but.addEventListener('click', eventFunc);
 }
 window.addEventListener('load', init);
